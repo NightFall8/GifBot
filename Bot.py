9aa0e334abc11a0ec1eb41eded5d66a78acbcafb
NightFall8 <alexander@stevens.net>
2026-03-24 16:01:22 -0400

    Moved config into the startup section.

--- Bot.py
+++ Bot.py
@@ -2,8 +2,10 @@ import discord, requests, random, io, numpy as np, imageio.v2 as imageio, json
 from discord.ext import commands
 from PIL import Image, ImageSequence
 
-config = json.load(open("config.json"))
+
 # ==========================================
+
+config = json.load(open("config.json"))
 
 intents = discord.Intents.default()
 intents.message_content = True
